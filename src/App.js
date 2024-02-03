6f032dc58a940dc6593d56c9e7cc292645f0da37
alish-ba15 <alishbazia390@gmail.com>
2024-02-03 21:12:50 +0500

    DOne

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="box">
+        <div className="title-box">
+          <img src="https://static.xx.fbcdn.net/rsrc.php/y1/r/4lCu2zih0ca.svg" alt="facebook" />
+          <p>Facebook helps you connect and share with the people in your life.</p>
+        </div>
+        <div className="form-box">
+          <form action="#">
+            <input type="email" placeholder="Email address or phone number" required />
+            <input type="password" placeholder="Password" required />
+            <button type="submit">Log in</button>
+            <div className="fog"><a href="#">Forgotten Password</a></div>
+
+            <hr />
+
+            <div className="Create">
+              <button type="submit">Create new account</button>
+            </div>
+          </form>
+
+          <div className="below-box">
+            <p><a href="#">Create a page</a> for a celebrity, brand, or business.</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
